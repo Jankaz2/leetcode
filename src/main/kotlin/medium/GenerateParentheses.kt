8be1334b4bfa3529c99fb47c9c6f923214a897cd
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-05-22 17:24:33 +0200

    GenerateParentheses

---
+++ src/main/kotlin/medium/GenerateParentheses.kt
@@ -0,0 +1,30 @@
+package medium
+
+
+class GenerateParentheses {
+
+    fun generateParenthesis(n: Int): List<String> {
+        val res: MutableList<String> = ArrayList()
+        recurse(res, 0, 0, "", n)
+        return res
+    }
+
+    private fun recurse(res: MutableList<String>, left: Int, right: Int, s: String, n: Int) {
+        println("recurse called for : $s left : $left right : $right")
+        if (s.length == n * 2) {
+            res.add(s)
+            return
+        }
+        if (left < n) {
+            recurse(res, left + 1, right, "$s(", n)
+        }
+        if (right < left) {
+            recurse(res, left, right + 1, "$s)", n)
+        }
+    }
+}
+
+fun main() {
+    val solution = GenerateParentheses()
+    solution.generateParenthesis(3)
+}
