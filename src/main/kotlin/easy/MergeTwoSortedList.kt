3325c818f54ee0d0b1cfff9fff3431f3a4a77c07
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-05-23 18:17:36 +0200

    ReverseALinkedList

--- src/main/kotlin/easy/MergeTwoSortedList.kt
+++ src/main/kotlin/easy/MergeTwoSortedList.kt
@@ -1,8 +1,6 @@
 package easy
 
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-}
+import ListNode
 
 class Solution {
     fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
