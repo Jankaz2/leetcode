6fbb2c5f338e95c3acd5b8f90ba88f694109d9bb
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-06-07 21:18:28 +0200

    MaximumDepthOfBinaryTree

--- src/main/kotlin/easy/MaximumDepthOfBinaryTree.kt
+++ src/main/kotlin/easy/MaximumDepthOfBinaryTree.kt
@@ -1,6 +1,7 @@
 package easy
 
 import TreeNode
+import java.util.Stack
 
 class MaximumDepthOfBinaryTree {
     fun maxDepth(root: TreeNode?): Int {
@@ -11,6 +12,42 @@ class MaximumDepthOfBinaryTree {
         val rightDepth = maxDepth(root.right)
         return 1 + maxOf(leftDepth, rightDepth)
     }
+
+    fun bfs(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        var level = 0
+        val queue = ArrayDeque<TreeNode>()
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            for (i in 0 until queue.size) {
+                val node = queue.removeLast()
+                node.left?.let { queue.add(it) }
+                node.right?.let { queue.add(it) }
+            }
+            level++
+        }
+
+        return level
+    }
+
+    fun ifs(root: TreeNode?): Int {
+        var max = 0
+        val stack = Stack<Pair<TreeNode?, Int>>()
+        stack.push(Pair(root, 1))
+        while (stack.isNotEmpty()) {
+            val (node, depth) = stack.pop()
+            node?.let {
+                max = max.coerceAtLeast(depth)
+                stack.add(Pair(it.left, depth + 1))
+                stack.add(Pair(it.right, depth + 1))
+            }
+
+        }
+
+        return max
+    }
+
 }
 
 fun main() {
@@ -20,5 +57,7 @@ fun main() {
     root.right = TreeNode(20)
     root.right!!.left = TreeNode(15)
     root.right!!.right = TreeNode(7)
-    solution.maxDepth(root)
+    println(solution.maxDepth(root))
+    println(solution.bfs(root))
+    println(solution.ifs(root))
 }
