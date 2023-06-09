56fe06b0a6b643fd12861b8a5a6e8ec5b6e35238
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-06-09 17:19:12 +0200

    ValidateBST

---
+++ src/main/kotlin/medium/ValidateBST.kt
@@ -0,0 +1,16 @@
+package medium
+
+import TreeNode
+
+class ValidateBST {
+    fun isValidBST(root: TreeNode?): Boolean {
+        if (root == null) return true
+        return valid(root, Int.MIN_VALUE, Int.MAX_VALUE)
+    }
+
+    private fun valid(root: TreeNode?, left: Int, right: Int): Boolean {
+        if (root == null) return true
+        if (!(root.`val` > left && root.`val` < right)) return false
+        return valid(root.left, left, root.`val`) && valid(root.right, root.`val`, right)
+    }
+}
