fdbe0f8db5a2641ad9eba50929fd795275c8685c
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-06-09 12:32:51 +0200

    LowestCommonAncestorOfABinarySearchTree

---
+++ src/main/kotlin/medium/LowestCommonAncestorOfABinarySearchTree.kt
@@ -0,0 +1,22 @@
+package medium
+
+import TreeNode
+
+class LowestCommonAncestorOfABinarySearchTree {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        var current = root
+
+        while (current != null) {
+            current = if (p!!.`val` > current.`val` && q!!.`val` > current.`val`) {
+                current.right
+            } else if (p.`val` < current.`val` && q!!.`val` < current.`val`) {
+                current.left
+            } else {
+                return current
+            }
+        }
+
+        return current
+    }
+}
+
