af61f60ba6ecd52a1930ef0d380c949b83aaa968
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-07-20 22:26:59 +0200

    ClimbingStairs

---
+++ src/main/kotlin/1d_dp/ClimbingStairs.kt
@@ -0,0 +1,15 @@
+package `1d_dp`
+
+class ClimbingStairs {
+    fun climbStairs(n: Int): Int {
+        var one = 1
+        var two = 1
+        for (s in 0 until n - 1) {
+            val temp = one
+            one += two
+            two = temp
+        }
+
+        return one
+    }
+}
