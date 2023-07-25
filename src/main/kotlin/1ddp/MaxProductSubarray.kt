3d7857fceec3e6805b6cb933a56ad91b8d49ad45
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-07-25 20:32:27 +0200

    MaxProductSubarray

---
+++ src/main/kotlin/1ddp/MaxProductSubarray.kt
@@ -0,0 +1,28 @@
+package `1ddp`
+
+class MaxProductSubarray {
+    fun maxProduct(nums: IntArray): Int {
+        var res = nums[0]
+        var curMin = 1
+        var curMax = 1
+
+        for (num in nums) {
+            val candidates = listOf(
+                num,
+                num * curMax,
+                num * curMin
+            )
+
+            curMax = candidates.max()
+            curMin = candidates.min()
+            res = maxOf(res, curMax)
+        }
+
+        return res
+    }
+}
+
+fun main() {
+    val solution = MaxProductSubarray()
+    solution.maxProduct(intArrayOf(2, 3, 2, 0, -3, -6, 4))
+}
