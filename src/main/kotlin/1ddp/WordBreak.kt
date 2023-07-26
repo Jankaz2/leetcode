d00f1a8a83e0aba1a4961c7df3b4f7af7e13dc6a
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-07-26 17:10:28 +0200

    WordBreak

---
+++ src/main/kotlin/1ddp/WordBreak.kt
@@ -0,0 +1,27 @@
+package `1ddp`
+
+class WordBreak {
+    fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        if (s.isEmpty() || wordDict.isEmpty()) return false
+
+        val dp = BooleanArray(s.length + 1)
+        dp[dp.lastIndex] = true
+
+        for (i in s.length - 1 downTo 0) {
+            for (word in wordDict) {
+                if (i + word.length <= s.length && s.substring(i, i + word.length) == word) {
+                    dp[i] = dp[i + word.length]
+                }
+
+                if (dp[i]) break
+            }
+        }
+
+        return dp[0]
+    }
+}
+
+fun main() {
+    val solution = WordBreak()
+    solution.wordBreak("applepenapple", listOf("apple", "pen"))
+}
