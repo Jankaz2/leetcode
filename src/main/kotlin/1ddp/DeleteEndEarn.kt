1c403eddac466e5e5f52fffb73ab3056cdd86240
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-07-28 19:41:02 +0200

    DeleteEndEarn

---
+++ src/main/kotlin/1ddp/DeleteEndEarn.kt
@@ -0,0 +1,29 @@
+package `1ddp`
+
+class DeleteEndEarn {
+    data class ValueCount(val value: Int, var count: Int)
+
+    fun deleteAndEarn(nums: IntArray): Int {
+        val max = nums.max()
+        val store = IntArray(max + 1) { 0 }
+
+        for (num in nums) {
+            store[num] += num
+        }
+
+        val dp = IntArray(store.size)
+        dp[1] = 1 * store[1]
+
+        for (i in 2 until dp.size) {
+            dp[i] = maxOf(dp[i - 2] + store[i], dp[i - 1])
+        }
+
+        return dp[dp.lastIndex]
+    }
+}
+
+fun main() {
+    val solution = DeleteEndEarn()
+    solution.deleteAndEarn(intArrayOf(3, 4, 2))
+    solution.deleteAndEarn(intArrayOf(2, 2, 3, 3, 3, 4, 4))
+}
--- src/main/kotlin/1ddp/HouseRobber.kt
+++ src/main/kotlin/1ddp/HouseRobber.kt
@@ -1,5 +1,7 @@
 package `1ddp`
 
+import kotlin.math.max
+
 class HouseRobber {
     fun rob(nums: IntArray): Int {
         if (nums.isEmpty()) return 0
@@ -15,6 +17,21 @@ class HouseRobber {
 
         return prev1
     }
+
+    fun rob2(nums: IntArray): Int {
+        if (nums.size < 2) return nums[0]
+
+        val dp = IntArray(nums.size)
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0].toDouble(), nums[1].toDouble()).toInt()
+
+        for (i in 2 until nums.size) {
+            dp[i] = max((dp[i - 2] + nums[i]).toDouble(), dp[i - 1].toDouble()).toInt()
+        }
+        return dp[nums.size - 1]
+    }
+
 }
 
 fun main() {
