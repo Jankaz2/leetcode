402b89c2b6f03641bf39425bfbf6b04977910692
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-07-29 16:35:33 +0200

    CombinationSum4

---
+++ src/main/kotlin/1ddp/CombinationSum4.kt
@@ -0,0 +1,23 @@
+package `1ddp`
+
+class CombinationSum4 {
+    fun combinationSum4(nums: IntArray, target: Int): Int {
+        val dp = IntArray(target + 1)
+
+        dp[0] = 1
+        for (i in 1..target) {
+            for (n in nums) {
+                if (i - n >= 0) {
+                    dp[i] += dp[i - n]
+                }
+            }
+        }
+
+        return dp[target]
+    }
+}
+
+fun main() {
+    val solution = CombinationSum4()
+    solution.combinationSum4(intArrayOf(1, 2, 3), 4)
+}
