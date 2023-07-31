683d383379f639ff47ac7c36c06683f70a6ba5e9
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-07-31 17:42:11 +0200

    PerfectSquares

---
+++ src/main/kotlin/1ddp/PerfectSquares.kt
@@ -0,0 +1,24 @@
+package `1ddp`
+
+class PerfectSquares {
+
+    fun numSquares(n: Int): Int {
+        val dp = IntArray(n + 1) { n }
+        dp[0] = 0
+
+        for (target in 1 .. n) {
+            for (s in 1 .. target) {
+                val squared = s * s
+                if (squared > target) break
+                dp[target] = minOf(dp[target], 1 + dp[target - squared])
+            }
+        }
+
+        return dp[n]
+    }
+}
+
+fun main() {
+    val solution = PerfectSquares()
+    solution.numSquares(12)
+}
