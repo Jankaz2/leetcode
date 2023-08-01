6d67b127891c4272bfd871d7c9997cbd1c7698b2
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-08-01 19:46:11 +0200

    Tribonacci

---
+++ src/main/kotlin/1ddp/Tribonacci.kt
@@ -0,0 +1,21 @@
+package `1ddp`
+
+class Tribonacci {
+    fun tribonacci(n: Int): Int {
+        if (n == 0) return n
+
+        var first = 0
+        var second = 1
+        var third = 1
+
+        for (i in 3..n) {
+            val tempResult = first + second + third
+            val tempSecond = second
+            second = third
+            first = tempSecond
+            third = tempResult
+        }
+
+        return third
+    }
+}
