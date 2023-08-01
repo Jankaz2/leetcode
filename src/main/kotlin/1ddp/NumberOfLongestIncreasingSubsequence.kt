5b33223e1059cb3ef9cf1c4a9b794e0bd14192b9
Jan Kazmierczak <jkazmierczak.it@gmail.com>
2023-08-01 18:35:44 +0200

    NumberOfLongestIncreasingSubsequence

---
+++ src/main/kotlin/1ddp/NumberOfLongestIncreasingSubsequence.kt
@@ -0,0 +1,37 @@
+package `1ddp`
+
+class NumberOfLongestIncreasingSubsequence {
+    fun findNumberOfLIS(nums: IntArray): Int {
+        val dp = mutableMapOf<Int, Pair<Int, Int>>() // length of LIS, counter
+        var globalMaxLen = 0
+        var globalMaxCount = 0
+
+        for (i in nums.size - 1 downTo 0) {
+            var currentMaxLen = 1
+            var currentMaxCount = 1
+
+            for (j in i + 1 until nums.size) {
+                if (nums[j] > nums[i]) {
+                    val (lisLength, counter) = dp[j]!!
+                    if (lisLength + 1 > currentMaxLen) {
+                        currentMaxLen = lisLength + 1
+                        currentMaxCount = counter
+                    } else if (lisLength + 1 == currentMaxLen) {
+                        currentMaxCount += counter
+                    }
+                }
+            }
+
+            if (currentMaxLen > globalMaxLen) {
+                globalMaxLen = currentMaxLen
+                globalMaxCount = currentMaxCount
+            } else if (currentMaxLen == globalMaxLen) {
+                globalMaxCount += currentMaxCount
+            }
+
+            dp[i] = Pair(currentMaxLen, currentMaxCount)
+        }
+
+        return globalMaxCount
+    }
+}
